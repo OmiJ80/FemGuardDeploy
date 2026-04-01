20dc1c93c1db9e62ab48fa33ebd693662ce26810
OmiJ80 <omijadhav6080@gmail.com>
2026-04-01 20:45:24 +0530

    asssement resolvingn nncdjhbcdd c

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base URL for API
-const baseURL = import.meta.env.VITE_API_URL || (import.meta.env.PROD ? 'https://femguardbackenddeploy.onrender.com/api' : 'http://localhost:5000/api');
+const baseURL = import.meta.env.PROD ? 'https://femguardbackenddeploy.onrender.com/api' : 'http://localhost:5000/api';
 
 const api = axios.create({
     baseURL,
